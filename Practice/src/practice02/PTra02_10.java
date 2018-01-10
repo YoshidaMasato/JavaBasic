bedf0fb7c2c459b23aa8a36d31b05367ea835169
DESKTOP-UFNQ98L\LIKEIT_STUDENT <992210acedia@gmail.com>
2018-01-10 15:42:14 +0900

    02_10

--- Practice/src/practice02/PTra02_10.java
+++ Practice/src/practice02/PTra02_10.java
@@ -21,6 +21,9 @@ public class PTra02_10 {
 		 * ※ 日本円には小数点がありませんので、整数の値で出力してください(少数部分は切り捨て)
 		 * ※ プログラムは何行書いても良いです
 		 */
+		coffee *= 1.08;
+		snack *= 1.08;
 
+		System.out.println(moneyInHand - coffee * 2 - snack);
 	}
 }
