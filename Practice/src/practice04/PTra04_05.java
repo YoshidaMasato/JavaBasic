8c8ea00e6128ea956fa0b5eb7490a50a28c326a1
DESKTOP-UFNQ98L\LIKEIT_STUDENT <992210acedia@gmail.com>
2018-01-11 11:31:06 +0900

    04_05

--- Practice/src/practice04/PTra04_05.java
+++ Practice/src/practice04/PTra04_05.java
@@ -47,18 +47,48 @@ public class PTra04_05 {
 			if (cpuHand == 0) {
 				System.out.println("グー！");
 
+				switch (myHand) {
+				case 0:
+					//continue;
+					break;
+				case 1:
+					//continue;
+					break;
+				case 2:
+					winFlg = true;
+					break;
+				}
 
 
 			} else if (cpuHand == 1) {
 				System.out.println("チョキ！！");
 
+				switch (myHand) {
+				case 0:
+					winFlg = true;
+					break;
+				case 1:
+					//continue;
+					break;
+				case 2:
+//					continue;
+					break;
+				}
 
 
-			} else {
+			} else if(cpuHand == 2) {
 				System.out.println("パー！！");
-
-
-
+				switch (myHand) {
+				case 0:
+					//continue;
+					break;
+				case 1:
+					winFlg = true;
+					break;
+				case 2:
+					//continue;
+					break;
+				}
 			}
 
 		}
