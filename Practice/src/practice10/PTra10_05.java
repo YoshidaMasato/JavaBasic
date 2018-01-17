6e9167bd592aedabeb88902d172b6790495ad675
DESKTOP-UFNQ98L\LIKEIT_STUDENT <992210acedia@gmail.com>
2018-01-17 10:29:31 +0900

    10_06まで終了

--- Practice/src/practice10/PTra10_05.java
+++ Practice/src/practice10/PTra10_05.java
@@ -27,16 +27,16 @@ public class PTra10_05 {
 
 		// Carクラスを作成後に着手してください
 		// ★ Car型の変数carを宣言し、Carクラスのインスタンスを代入してください
-
+		Car car = new Car();
 
 		// ★ 変数carに格納されているインスタンスのserialNoフィールドに、10000を代入してください
-
+		car.serialNo = 10000;
 
 		// ★ 変数carに格納されているインスタンスのcolorフィールドに、"Red"を代入してください
-
+		car.color = "Red";
 
 		// ★ 変数carに格納されているインスタンスのgasolineフィールドに、50を代入してください
-
+		car.gasoline = 50;
 
 		// 目的地までの距離
 		final int distance = 300;
@@ -47,6 +47,19 @@ public class PTra10_05 {
 		 * ★ 目的地についた時点で「目的地にまでn時間かかりました。残りのガソリンは、xリットルです」を出力してください
 		 * ※n：runメソッドを実行した回数, xは残りのガソリンの数です
 		 */
+		int total = 0;
+		int n;
+		for(n = 0; distance > total; n++) {
+			int move = car.run();
+			total += move;
 
+			if(move == -1) {
+				System.out.println("目的地に到達できませんでした");
+				break;
+			}
+		}
+		if(car.gasoline != -1) {
+			System.out.println("目的地まで"+ n +"時間かかりました。残りのガソリンは、"+ car.gasoline +"リットルです");
+		}
 	}
 }
--- Practice/src/practice10/PTra10_06.java
+++ Practice/src/practice10/PTra10_06.java
@@ -18,6 +18,63 @@ public class PTra10_06 {
 		 * carインスタンスを3件作成し、それぞれの色、ガソリンを入力して決定してください
 		 * 各carインスタンスのrunメソッドを実行して、それぞれ「目的地にまでn時間かかりました。残りのガソリンは、xリットルです」を出力してください。
 		 */
+		Car car1 = new Car();
+		car1.color = "Red";
+		car1.gasoline = 50;
 
+		Car car2 = new Car();
+		car2.color = "Blue";
+		car2.gasoline = 40;
+
+		Car car3 = new Car();
+		car3.color = "Black";
+		car3.gasoline = 30;
+
+		final int distance = 300;
+
+		int total1 = 0;
+		int count1;
+		for(count1 = 0; distance > total1; count1++) {
+			int move = car1.run();
+			total1 += move;
+
+			if(move == -1) {
+				System.out.println("目的地に到達できませんでした");
+				break;
+			}
+		}
+		if(car1.gasoline != -1) {
+			System.out.println("目的地まで"+ count1 +"時間かかりました。残りのガソリンは、"+ car1.gasoline +"リットルです");
+		}
+
+		int total2 = 0;
+		int count2;
+		for(count2 = 0; distance > total2; count2++) {
+			int move = car2.run();
+			total2 += move;
+
+			if(move == -1) {
+				System.out.println("目的地に到達できませんでした");
+				break;
+			}
+		}
+		if(car2.gasoline != -1) {
+			System.out.println("目的地まで"+ count2 +"時間かかりました。残りのガソリンは、"+ car2.gasoline +"リットルです");
+		}
+
+		int total3 = 0;
+		int count3;
+		for(count3 = 0; distance > total3; count3++) {
+			int move = car3.run();
+			total3 += move;
+
+			if(move == -1) {
+				System.out.println("目的地に到達できませんでした");
+				break;
+			}
+		}
+		if(car3.gasoline != -1) {
+			System.out.println("目的地まで"+ count3 +"時間かかりました。残りのガソリンは、"+ car3.gasoline +"リットルです");
+		}
 	}
 }
