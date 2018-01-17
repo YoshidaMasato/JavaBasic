a423656a219fd512aca7fddc0572eb9a77092bdc
DESKTOP-UFNQ98L\LIKEIT_STUDENT <992210acedia@gmail.com>
2018-01-17 14:45:22 +0900

    11_03まで終了

--- Practice/src/practice11/PTra11_02.java
+++ Practice/src/practice11/PTra11_02.java
@@ -10,11 +10,13 @@ public class PTra11_02 {
 	public static void main(String[] args) {
 
 		// ★ ReadFileClassのクラスメソッドreadBookDataFile()を使い、本情報を取得します
-
+		Book[] bookList = FileReaderClass.readBookDataFile();
 
 		System.out.println("探したい本のタイトル（又はその一部）を入力してください");
 
-		// ★ ユーザが入力した文字列が、本情報のタイトル一部に含まれていた場合は、その本情報を出力してください
-
+//		 ★ ユーザが入力した文字列が、本情報のタイトル一部に含まれていた場合は、その本情報を出力してください
+		for(Book book : bookList) {
+			System.out.println(book.dispBookInfo());
+		}
 	}
 }
--- Practice/src/practice11/PTra11_03.java
+++ Practice/src/practice11/PTra11_03.java
@@ -11,21 +11,25 @@ public class PTra11_03 {
 	/*
 	 * ★ PTra11_03クラスに、クラスフィールドでint型のnumberを定義してください
 	 */
-
+	public static int number = 0;
 	/*
 	 * ★ PTra11_03クラスのコンストラクタを引数なしで定義してください
 	 * ★ 処理は、クラスフィールドnumberに1を加算してください
 	 */
+	PTra11_03(){
+		number += 1;
+	}
 
 	public static void main(String[] args) {
 
 		// ★ sumメソッドを呼び出してください
-
+		PTra11_03 act = new PTra11_03();
+		act.sum(3,4);
 	}
 
 	public void sum(int x, int y) {
 		System.out.println("sumメソッドを呼び出しました。" + x + "＋" + y + "＝" + (x + y));
 
-		System.out.println("クラスフィールドnumber：" /* + ★ クラスフィールドnumberの出力をしてください */);
+		System.out.println("クラスフィールドnumber：" + number/* + ★ クラスフィールドnumberの出力をしてください */);
 	}
 }
