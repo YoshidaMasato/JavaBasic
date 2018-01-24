a5a064b3cf52ec27fe9cad7e862d5210b4addd78
DESKTOP-TJLQVEH\s9922 <992210acedia@gmail.com>
2018-01-24 17:47:05 +0900

    17_04まで終了

--- Practice/src/practice17/ptra17/PTra17_04.java
+++ Practice/src/practice17/ptra17/PTra17_04.java
@@ -15,6 +15,14 @@ public class PTra17_04 {
 		 * ★ strに入っている文字列を1文字ずつゆっくり表示させるプログラムを完成させてください
 		 * ※ Thread.sleep を使用すること
 		 */
-
+		char[] array = str.toCharArray();
+		for (int i = 0; i < str.length(); i++) {
+			System.out.print(array[i]);
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				/* エラーが発生した場合はここにくる */
+			}
+		}
 	}
 }
