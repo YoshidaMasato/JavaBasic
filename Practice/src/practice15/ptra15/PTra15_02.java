5fd5c2f6197248a504f210df8a0c900cd8f97374
DESKTOP-TJLQVEH\s9922 <992210acedia@gmail.com>
2018-01-24 13:24:39 +0900

    15_02まで終了

--- Practice/src/practice15/ptra15/PTra15_02.java
+++ Practice/src/practice15/ptra15/PTra15_02.java
@@ -13,11 +13,14 @@ public class PTra15_02 {
 	public static void main(String[] args) {
 
 		Room room = new Java2Month();
+		Java2Month java2Month = new Java2Month();
 
 		// roomに格納されたJava2MonthインスタンスのgetCurriculumメソッドを呼び出してください
-
+		String[] array = java2Month.getCurriculum();
 
 		// 取得したカリキュラム情報を全て出力してください
-
+		for(String str : array) {
+			System.out.println(str);
+		}
 	}
 }
